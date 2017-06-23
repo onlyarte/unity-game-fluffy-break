02dfd06296fc451596b6fc5e539ce6ab9df651c8
Rostyslav Rozhko <rostyslav.rozhko@gmail.com>
2017-06-23 09:42:10 +0300

    added spikes

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        PlayerController.current.Death();
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        OnTriggerEnter2D(collider);
+    }
+}
