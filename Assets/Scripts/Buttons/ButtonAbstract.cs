4d0f0aae51e2e43598792a917b9e64b02aee538a
Rostyslav Rozhko <rostyslav.rozhko@gmail.com>
2017-06-23 08:10:48 +0300

    added class to work with buttons

---
+++ Assets/Scripts/Buttons/ButtonAbstract.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAbstract : MonoBehaviour {
+
+    public Sprite buttonPressed;
+    public Sprite buttonUnpressed;
+
+    private new SpriteRenderer renderer = null;
+
+    protected virtual void ButtonEnterAction()
+    {
+    }
+
+    protected virtual void ButtonExitAction()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        PressButton();
+        this.ButtonEnterAction();
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        OnTriggerEnter2D(collider);
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        UnpressButton();
+        this.ButtonExitAction();
+    }
+
+    void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void PressButton()
+    {
+        renderer.sprite = buttonPressed;
+    }
+
+    void UnpressButton()
+    {
+        renderer.sprite = buttonUnpressed;
+    }
+}
---
+++ Assets/Scripts/Buttons/TestButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButton : ButtonAbstract { 
+
+	protected override void ButtonEnterAction()
+    {
+
+    }
+
+    protected override void ButtonExitAction()
+    {
+
+    }
+
+}
