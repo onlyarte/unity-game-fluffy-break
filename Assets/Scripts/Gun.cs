3ae601ab45184c85b5fc6243d338ed70aa145565
Rostyslav Rozhko <rostyslav.rozhko@gmail.com>
2017-06-25 19:53:27 +0300

    added bomb on level2

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour {
-    GameObject Point;
-    float pointRange = 1.3f;
+    public Vector3 vec_top;
+    public Vector3 vec_bottom;
+    public float Speed = 1f;
+
+    Vector3 startingPosition;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Vector3 newPosition = getPlayerPosition() + new Vector3(2, 2, 0);
-        Point = Instantiate(Resources.Load("Prefabs/Point"), newPosition, transform.rotation) as GameObject;
+        startingPosition = transform.position;
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         if (isPlayerInRange())
         {
             Vector3 latePosition = getPlayerPosition();
@@ -22,18 +25,39 @@ public class Gun : MonoBehaviour {
         }
             else
         {
-            if (Point)
-            {
-                Destroy(Point);
-            }
         }
 	}
 
-    IEnumerator setPosition(Vector3 newPos)
+    IEnumerator setPosition(Vector3 playerPos)
     {
-        yield return new WaitForSeconds(.5f);
-        newPos.y += pointRange;
-        Point.transform.position = newPos;
+        yield return new WaitForSeconds(2f);
+        Vector3 pos = transform.position;
+        Vector3 newPos = pos;
+        float directionX;
+        float directionY;
+
+        if (pos.x <= playerPos.x)
+            directionX = 1;
+        else
+            directionX = -1;
+
+        if (pos.y <= playerPos.y)
+            directionY = 1;
+        else
+            directionY = -1;
+
+        if(playerPos.x != pos.x)
+        {
+            
+            newPos.x += Speed * directionX;
+        }
+
+        if (playerPos.y != transform.position.y)
+        {
+            newPos.y += Speed * directionY;
+        }
+
+        transform.position = newPos;
     }
 
     Vector3 getPlayerPosition()
@@ -43,12 +67,14 @@ public class Gun : MonoBehaviour {
 
     bool isPlayerInRange()
     {
-        return true;
+        Vector3 playerPosition = PlayerController.current.transform.position;
+        return playerPosition.x >= vec_bottom.x && playerPosition.x <= vec_top.x && playerPosition.y >= vec_bottom.y && playerPosition.y <= vec_top.y;
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("death");
+        transform.position = startingPosition;
+        PlayerController.current.Death();
     }
 
     void OnTriggerStay2D(Collider2D collider)
