6f7c028af09340a46c5cae1877e31b91a6f20bcd
Rostyslav Rozhko <rostyslav.rozhko@gmail.com>
2017-06-25 20:42:29 +0300

    added mainmenu

---
+++ Assets/Scripts/NGUI/PlayButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public MyButton playButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        playButton.signalOnClick.AddListener(this.ShowLevel);
+    }
+
+    void ShowLevel()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
